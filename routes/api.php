2a6694a7cdbe796a4c2c95a05a58444edd5164ec
Andrew Tolpeko <andriush@mail.ru>
2024-08-20 10:35:27 +0300

    Removed redundant files and code

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,6 @@ use App\Http\Controllers\VerificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('register',[SanctumAuthController::class, 'register']);
 Route::post('login',[SanctumAuthController::class, 'login']);
 Route::post('logout',[SanctumAuthController::class, 'logout'])->middleware('auth:sanctum');
